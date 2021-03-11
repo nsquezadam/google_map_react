e16877c4eaec5610650a9ef4574aefa993b4ea3e
Natalia Quezada <nsquezadam@gmail.com>
2021-03-10 22:32:25 -0300

    busquedor  coordenada

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import Map from './components/Map'
 
 
 
-
-
-
 const App = (props) => {
-  console.log(props);
+
   const apiKey = "key=AIzaSyCGIRHlHQX_miCOxHGMs0I9G4gydRT3Eek";
   const mapURl = `https://maps.googleapis.com/maps/api/js?${apiKey}&v=3.exp&libraries=geometry,drawing,places`
 
@@ -17,7 +14,7 @@ const App = (props) => {
    <Map 
    googleMapURL={mapURl}
    loadingElement={<p>Cargando...</p>}
-   containerElement={<div style={{ height: `50vh` }} />}
+   containerElement={<div style={{ height: `100vh` }} />}
    mapElement={<div style={{ height: `100%` }} />} 
     
     />
--- src/components/Map.js
+++ src/components/Map.js
@@ -14,29 +14,48 @@ const Map = () => {
     longitude : ''
   })
   const handleInputChange = (e) => {
-    console.log(e.target.name)
-     console.log(e.target.value)
+    
     setDataCoordinate({
         ...dataCoordinates,
         [e.target.name] : e.target.value
+        
     })
+    // console.log(typeof("tipo", e.target.value))
 }
+const enviarDatos = (e) => {
+  e.preventDefault()
+  console.log('enviando datos...' + parseFloat(dataCoordinates.latitude)  + ' ' + parseFloat(dataCoordinates.longitude)
+  
+  )};
  
-
+// const latNumber = parseFloat(dataCoordinates.latitude) 
+// const lngNumber = parseFloat(dataCoordinates.longitude) 
+// console.log(typeof('nuevas const',latNumber, lngNumber));
+// console.log(latNumber, lngNumber);
   return (
     <>
     <ContainerSearch>
-       <input type="text" placeholder="Latitud" name="Latitude"  onChange={(e)=>handleInputChange(e.target.value)}/>
-       <input type="text" placeholder="Longitud" name="Longitude"  onChange={(e)=>handleInputChange(e.target.value)}/>
+      <Form onSubmit={enviarDatos}>
+        
+     
+       <Input type="text" placeholder="Latitud"  onChange={handleInputChange} name="latitude"/>
+       <Input type="text" placeholder="Longitud" onChange={handleInputChange} name="longitude"/>
+       <Button type="submit">Buscar</Button>
+       </Form>
+      {/* <p>{dataCoordinates.latitude}</p>
+      <p>{dataCoordinates.longitude}</p> */}
     </ContainerSearch>
-
+   
     <GoogleMap
     defaultZoom={15}
     defaultCenter={{ lat: -33.436884, lng: -70.634395 }}
   >
     <Marker
+      position={{ lat: parseFloat(dataCoordinates.latitude), lng:parseFloat(dataCoordinates.longitude)
+          }}/>
+    {/* <Marker
       position={{ lat: -33.436884, lng: -70.634395 }}
-    />
+    /> */}
   </GoogleMap>
     </>
   );
@@ -52,6 +71,35 @@ flex-direction: row;
 justify-content:center;
 align-items: center;
 align-content:center;
+position:fixed;
+top:2vh;
+z-index:3;
+`
+const Form = styled.form`
+width:50vw;
+height:40px;
+display:flex;
+justify-content:center;
+flex-wrap:nowrap;
+align-content:center;
+padding: 5px;
+`
+
+const Input = styled.input`
+width:150px;
+height:30px;
+padding: 10px;
+font-size:2rem;
+margin:10px;
+`
+
+const Button = styled.button`
+width:200px;
+height:50px;
+margin-top:10px;
+padding: 10px;
+background-color:var(--dark-blue);
+color: var(--white);
 `
 
 export default  withScriptjs (
